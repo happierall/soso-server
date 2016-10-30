4daecff2b1cf37c26cfdf218f1186b1b781264ed
Ruslan Yanberdin <happierall@gmail.com>
2016-10-31 00:01:41 +0300

    change examples

--- examples/advanced/views/users.go
+++ examples/advanced/views/users.go
@@ -1,24 +1,26 @@
 package views
 
+import soso "github.com/happierall/soso-server"
+
 func init() {
-  Routes.Add("users", "retrieve", UserRetrieve)
-  Routes.Add("users", "create", UserCreate)
+	Routes.Add("users", "retrieve", UserRetrieve)
+	Routes.Add("users", "create", UserCreate)
 }
 
 func UserRetrieve(m *soso.Msg) {
 
-  m.Success(map[string]interface{}{
-    "id": m.RequestMap["id"],
-    "Name": "Mary"
-  })
+	m.Success(map[string]interface{}{
+		"id":   m.RequestMap["id"],
+		"Name": "Mary",
+	})
 
 }
 
 func UserCreate(m *soso.Msg) {
 
-  m.Success(map[string]interface{}{
-    "message": "message hi",
-    "id": m.RequestMap["id"],
-  })
+	m.Success(map[string]interface{}{
+		"message": "message hi",
+		"id":      m.RequestMap["id"],
+	})
 
 }
